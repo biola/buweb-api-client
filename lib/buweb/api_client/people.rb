9107c98fd51b2f44d3e7b5456e77a168bef7dc6c
Jeff Silzer <jeff.silzer@biola.edu>
2014-04-25 11:05:41 -0700

    BUWeb::APIClient::People#by_id is now #show

--- lib/buweb/api_client/people.rb
+++ lib/buweb/api_client/people.rb
@@ -2,16 +2,13 @@ module BUWeb
   module APIClient
     class People < Weary::Client
       domain BUWebAPIClient.config.base_url
-      # use Weary::Middleware::ContentType
       use Weary::Middleware::HMACAuth, [BUWebAPIClient.config.credentials]
 
       get :index, '/people' do |resource|
         resource.optional :affiliation
       end
 
-      get :by_id, '/people/{id}' do |resource|
-        resource.optional :type
-      end
+      get :show, '/people/{id}'
     end
   end
 end
